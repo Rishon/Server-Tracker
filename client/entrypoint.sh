af1b4b8e90bdac88afdd3f57c43232c06f73609d
Rishon Jaffe <rishonjaffe9@gmail.com>
2026-02-06 12:01:51 +0200

    Optimize dockerfile & GH Workflow

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,7 +2,5 @@
 
 FRONTEND_PORT="${FRONTEND_PORT:-3006}"
 
-~/.bun/bin/bun install
-~/.bun/bin/bun next telemetry disable
-~/.bun/bin/bun run build
-~/.bun/bin/bun run start -- -H 0.0.0.0 -p "$FRONTEND_PORT"
+bun next telemetry disable
+bun run start -- -H 0.0.0.0 -p "$FRONTEND_PORT"
